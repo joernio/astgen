e3ce7ce1b375578a2aec61f9c92d84a7998697d7
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2025-05-27 14:44:46 +0200

    Update dependencies and remove unused tsc options (#35)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    transform: { '^.+\\.ts?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }] },
+    testRegex: '/test/.+\\.test\\.ts$'
 };
