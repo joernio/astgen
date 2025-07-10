cbe8ac1980379adcaaccc0724ad6395e33b0567f
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2025-07-10 12:25:07 +0200

    Simplifications for typemap generation and test fixtures

--- src/TscUtils.ts
+++ src/TscUtils.ts
@@ -26,10 +26,31 @@ function safeTypeToString(node: tsc.Type, typeChecker: tsc.TypeChecker): string 
     }
 }
 
+function isSignatureDeclaration(node: tsc.Node): node is tsc.SignatureDeclaration {
+    return tsc.isSetAccessor(node) || tsc.isGetAccessor(node) ||
+        tsc.isConstructSignatureDeclaration(node) || tsc.isMethodDeclaration(node) ||
+        tsc.isFunctionDeclaration(node) || tsc.isConstructorDeclaration(node)
+}
+
 export function typeMapForFile(file: string): TypeMap {
     function addType(node: tsc.Node): void {
         if (tsc.isSourceFile(node)) return;
-        const typeStr = safeTypeToString(typeChecker.getTypeAtLocation(node), typeChecker);
+        let typeStr;
+        if (isSignatureDeclaration(node)) {
+            const signature: tsc.Signature = typeChecker.getSignatureFromDeclaration(node)!;
+            const returnType: tsc.Type = typeChecker.getReturnTypeOfSignature(signature);
+            typeStr = safeTypeToString(returnType, typeChecker);
+        } else if (tsc.isFunctionLike(node)) {
+            const funcType: tsc.Type = typeChecker.getTypeAtLocation(node);
+            const funcSignature: tsc.Signature = typeChecker.getSignaturesOfType(funcType, tsc.SignatureKind.Call)[0];
+            if (funcSignature) {
+                typeStr = safeTypeToString(funcSignature.getReturnType(), typeChecker);
+            } else {
+                typeStr = safeTypeToString(typeChecker.getTypeAtLocation(node), typeChecker);
+            }
+        } else {
+            typeStr = safeTypeToString(typeChecker.getTypeAtLocation(node), typeChecker);
+        }
         if (typeStr !== Defaults.ANY) {
             const pos = `${node.getStart()}:${node.getEnd()}`;
             seenTypes.set(pos, typeStr);
